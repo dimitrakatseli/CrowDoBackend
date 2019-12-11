1415bd0c72e8c6ac3729301a4e7943b1641b232f
EleniKoutra <57862398+EleniKoutra@users.noreply.github.com>
2019-12-11 14:39:45 +0200

    Update DbContext.cs

--- CrowDo/Repository/DbContext.cs
+++ CrowDo/Repository/DbContext.cs
@@ -1,11 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
-namespace CrowDo.Repository
+namespace CrowDo
 {
-    public class DbContext
+    class CrowDoDB : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Package> Packages { get; set; }
+        public DbSet<Funding> Fundings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connString1 = "Server =localhost ; Database = CrowDo; Integrated Security = SSPI; Persist Security Info=False;";
+            optionsBuilder.UseSqlServer(connString1);
+        }
+
     }
+
 }
