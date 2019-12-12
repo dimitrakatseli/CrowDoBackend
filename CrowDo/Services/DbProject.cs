8a431c966009ee68b48d5914a0f314038746aa80
EleniKoutra <57862398+EleniKoutra@users.noreply.github.com>
2019-12-12 10:43:55 +0200

    Update DbProject.cs

--- CrowDo/Services/DbProject.cs
+++ CrowDo/Services/DbProject.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,7 +8,31 @@ namespace CrowDo.Services
     public class DbProject
     {
 
-        public void AddProject(int id, Project project)
+      
+             public static List<Project> GetListOfProjects()
+        {
+            using (var db = new CrowDoDB())
+            {
+
+                return db.Projects.ToList();
+            }
+
+        }
+
+        public static Project GetProject(string id)
+        {
+
+            using (var db = new CrowDoDB())
+            {
+
+                return db.Projects.Where(p => p.ProjectID.Equals(id)).FirstOrDefault();
+
+            }
+
+        }
+
+
+        public static void AddProject(int id, Project project)
         {
             using (var db = new CrowDoDB())
             {
@@ -37,8 +61,7 @@ namespace CrowDo.Services
                 }
 
             }
-
-
+            
 
         }
 
